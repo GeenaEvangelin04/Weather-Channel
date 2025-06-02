b0f99767e9519c4aff73b697647ad2f0943af1ac
GeenaEvangelin04 <evangelingeena@gmail.com>
2025-06-02 15:26:30 +0530

    Update project with latest changes

--- netlify/functions/getWeather.js
+++ netlify/functions/getWeather.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 exports.handler = async function(event, context) {
   const city = event.queryStringParameters.city;
-  const API_KEY = process.env.API_KEY;
+  const API_KEY = process.env.REACT_APP_API_KEY;
 
   try {
     const response = await axios.get(
